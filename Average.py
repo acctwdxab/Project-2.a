d12206e06e908029c29f5af051777f3bf65f7607
acctwdxab <71532324+acctwdxab@users.noreply.github.com>
2020-10-03 23:44:23 -0700

    Add files via upload

---
+++ Average.py
@@ -0,0 +1,8 @@
+print('Please enter five numbers.')
+num1=float(input())
+num2=float(input())
+num3=float(input())
+num4=float(input())
+num5=float(input())
+result=(num1+num2+num3+num4+num5)/5
+print(result)
