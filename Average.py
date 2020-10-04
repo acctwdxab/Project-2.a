5dbc4bdbd5c432f17f7f33104e092284cf7a1cf0
acctwdxab <71532324+acctwdxab@users.noreply.github.com>
2020-10-04 00:22:37 -0700

    Delete Average.py

--- Average.py
+++
@@ -1,9 +0,0 @@
-print('Please enter five numbers.')
-num1=float(input())
-num2=float(input())
-num3=float(input())
-num4=float(input())
-num5=float(input())
-print('The average of those numbers is:')
-result=(num1+num2+num3+num4+num5)/5
-print(result)
